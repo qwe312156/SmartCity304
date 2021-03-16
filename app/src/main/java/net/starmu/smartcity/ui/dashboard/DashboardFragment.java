cfb48e97224a8735209f2aae7ff9d99b88e33b46
Cghk002 <3288889xt>
2021-03-16 14:15:24 +0800

    sss

--- app/src/main/java/net/starmu/smartcity/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/net/starmu/smartcity/ui/dashboard/DashboardFragment.java
@@ -15,21 +15,11 @@ import androidx.lifecycle.ViewModelProviders;
 import net.starmu.smartcity.R;
 
 public class DashboardFragment extends Fragment {
-
-    private DashboardViewModel dashboardViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
--- app/src/main/java/net/starmu/smartcity/ui/home/HomeFragment.java
+++ app/src/main/java/net/starmu/smartcity/ui/home/HomeFragment.java
@@ -15,21 +15,11 @@ import androidx.lifecycle.ViewModelProviders;
 import net.starmu.smartcity.R;
 
 public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
